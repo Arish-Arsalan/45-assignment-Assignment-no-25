e7aa994e300c4e5b81d9d93921a751e063485354
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-04-04 05:39:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+//choose a color for an alien as you
+//did in Exercise 25, and write an if-else chain.
+let alien_color = "green";
+//
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
---
+++ app.ts
@@ -0,0 +1,9 @@
+//choose a color for an alien as you
+//did in Exercise 25, and write an if-else chain.
+let alien_color:string ="green";
+
+//
+
+if(alien_color =="green"){
+    console.log("the player just earned 5 points.");
+}
